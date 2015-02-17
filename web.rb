304ca4f79fe5ed7ada71692af6aa4557633b8344
haya14busa <hayabusa1419@gmail.com>
2015-02-17 22:56:17 +0900

    Yaml as JSON!

--- web.rb
+++ web.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'sinatra/jsonp'
+
+require 'open-uri'
+require 'yaml'
 
 get '/' do
   'Hello, world'
 end
+
+get '/yaml' do
+  url = params[:url]
+  return 'yaml' if !url
+  jsonp YAML.load(open(url).read)
+end
+
